4e913db9be466174bd7608b971809efd3b25ad96
Elmira Arutiynian <elliejade316@gmail.com>
2023-05-24 04:54:19 -0400

    update

--- cartoon.js
+++ cartoon.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
@@ -32,4 +33,40 @@ for(var i = 0; i < canvas.width; i += 20) {
 
 ctx.font = '30px Arial';
 ctx.fillStyle = 'blue';
+=======
+
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+//background color
+ctx.fillStyle = '#87ceeb';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//Create shape and add for loop to repeat items
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, 2 * Math.PI, false);
+ctx.fillStyle = 'yellow';
+ctx.fill();
+ctx.lineWidth = 5;
+ctx.strokeStyle = '#ff9933';
+ctx.stroke();
+
+
+ctx.fillStyle = '#228b22';
+ctx.fillRect(0, 400, canvas.width, 200);
+
+ctx.fillStyle = '#8b4513';
+ctx.fillRect(300, 300, 200, 100);
+ctx.fillStyle = 'white';
+ctx.fillRect(350, 350, 50, 50);
+
+
+for(var i = 0; i < canvas.width; i += 20) {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(i, 590, 10, 10);
+}
+
+ctx.font = '30px Arial';
+ctx.fillStyle = 'blue';
+>>>>>>> 5297ec63559ccda6685e46e66443642a708e3871
 ctx.fillText('First cartoon project made by Elmira!', 200, 50);
--- chart.js
+++ chart.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //Elmira Arutiynian, we will first create a laberls array that will hold each of the data points
 //Next, datasets will hold the array of objects 
 //Next I will be adding color in order for to differ each one of the labels
@@ -38,4 +39,46 @@ var myBarChart = new Chart(ctx, {
     options: {
         indexAxis: 'y',  
     }
+=======
+//Elmira Arutiynian, we will first create a laberls array that will hold each of the data points
+//Next, datasets will hold the array of objects 
+//Next I will be adding color in order for to differ each one of the labels
+const data = {
+    labels: ['Apples', 'Oranges', 'Bananas', 'Strawberries', 'Grapes'],
+    datasets: [{
+        label: 'Elmira Arutiynian',
+        data: [16, 29, 17, 5, 2],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+            'rgb(255, 100, 131)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(153, 111, 255)'
+        ],
+        borderWidth: 1
+    }]
+};
+//this creates doughnut chart
+var ctx = document.getElementById('doughnutChart').getContext('2d');
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: {}
+});
+//this creates bar chart
+ctx = document.getElementById('barChart').getContext('2d');
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        indexAxis: 'y',  
+    }
+>>>>>>> 5297ec63559ccda6685e46e66443642a708e3871
 });
